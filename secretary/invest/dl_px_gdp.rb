52af78a782a3041d273805e129edd00d53fdd546
Jiri Stovicek <Jirka@Jiris-MacBook-Pro-7.local>
2014-09-14 13:37:12 +0200

    dir structure update

--- secretary/invest/dl_px_gdp.rb
+++ secretary/invest/dl_px_gdp.rb
@@ -27,7 +27,8 @@ end
 
 # download data
 data = String.new
-query = "select gdp.day, gdp.value gdp_value, px.value px_value from gdp, px where px.day = (select px.day from px where px.day <= gdp.day and px.value is not null order by px.day desc limit 1)"
+#query = "select gdp.day, gdp.value gdp_value, px.value px_value from gdp, px where px.day = (select px.day from px where px.day <= gdp.day and px.value is not null order by px.day desc limit 1)"  # selects just days with gdp filled
+query = "select day, px_value, gdp_value from ( select px.day, px.value px_value, gdp.value / 1000 gdp_value from px left outer join gdp on gdp.day = px.day union select gdp.day, px.value px_value, gdp.value / 1000 gdp_value from gdp left outer join px on gdp.day = px.day where px.value is null) temp order by day"  # full outer join of px and gdp
 result = db.query(query)
 result.each { |x| data += x.join(',') + "\n" }
 
